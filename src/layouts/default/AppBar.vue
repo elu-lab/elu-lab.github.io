e4a10200331ee305ed236906986863732f96633b
Bugeun Kim <bgnkim@cau.ac.kr>
2023-04-20 04:03:39 +0000

    Fix for wrong logo image size

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 v-app-bar(flat, color='indigo-darken-3', elevation=1, density="compact")
   v-app-bar-title
-    v-img(src="/white-LI.png", height="100%")
+    v-img(src="/white-LI.png", height="28px")
     router-link.text-white.text-decoration-none(:to="'/' + lang", active-class="indigo-lighten-2") ELU Lab @ CAU
   template(v-slot:extension)
     v-spacer
